3533761a6c42bb8715cc3e15cfe0a46b1cce0618
Saulo <castro1799804@gmail.com>
2026-09-10 01:55:45 -0300

    feat: deixando basicamente equivalente ao que tinha em python antes

--- bacalhau.c
+++ bacalhau.c
@@ -4,10 +4,70 @@
 #include <float.h>
 
 typedef struct {
-    float x;
-    float y;
+    double x;
+    double y;
     int id;
 } Ponto;
+
+typedef struct {
+    int u;
+    int v;
+} Par;
+
+typedef struct {
+    double dist;
+    Par* pares;
+    int qtd;
+} Resultado;
+
+double distancia(Ponto p1, Ponto p2) {
+    return sqrt(pow((p1.x - p2.x), 2) + pow((p1.y - p2.y), 2));
+}
+
+void adicionarPar(Resultado* res, int id1, int id2) {
+    res->pares = realloc(res->pares, (res->qtd+1)*sizeof(Par));
+    res->pares[res->qtd].u = (id1 < id2) ? id1 : id2;
+    res->pares[res->qtd].v = (id1 < id2) ? id1 : id2;
+    res->qtd++;
+ }
+
+Resultado acharMenoresDistancias(Ponto* pontos, int inicio, int fim) {
+    int tamanho = inicio - fim + 1;
+    Resultado res;
+    res.dist = DBL_MAX;
+    res.pares = NULL;
+    res.qtd = 0;
+
+    if (tamanho == 2) {
+        res.dist = distancia(pontos[inicio], pontos[fim]);
+        adicionarPar(&res, pontos[inicio].id, pontos[fim].id);
+        return res;
+    }
+
+    int meio = inicio + (fim - inicio)/2;
+    Resultado esq = acharMenoresDistancias(pontos, inicio, meio);
+    Resultado dir = acharMenoresDistancias(pontos, meio + 1, fim);
+    
+}
+
+// Função auxiliar que compara as coordenadas x para usar no qsort
+int comparaX(const void* a, const void* b) {
+    Ponto* p1 = (Ponto*)a;
+    Ponto* p2 = (Ponto*)b;
+
+    if (p1->x < p2->x) return -1;
+    if (p1->x > p2->x) return 1;
+    return 0;
+}
+
+int comparaY(const void* a, const void* b) {
+    Ponto* p1 = (Ponto*)a;
+    Ponto* p2 = (Ponto*)b;
+
+    if (p1->y < p2->y) return -1;
+    if (p1->y > p2->y) return 1;
+    return 0;
+}
 
 int main() {
 
@@ -16,9 +76,12 @@ int main() {
     Ponto* pontos = (Ponto*)malloc(n*sizeof(Ponto));
 
     for (int i = 0; i < n; i++) {
-        scanf("%f %f", &pontos[i].x, &pontos[i].y);
+        scanf("%lf %lf", &pontos[i].x, &pontos[i].y);
         pontos[i].id = i;
     }
+
+    // qsort é O(nlogn)
+    qsort(pontos, n, sizeof(Ponto), comparaX);
 
     free(pontos);
     return 0;
